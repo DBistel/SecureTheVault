90c142b45f5221322f456d87cd767bff91337316
dbistel <ddbistel@gmail.com>
2025-05-29 19:51:22 -0600

    added JS

---
+++ index.js
@@ -0,0 +1,17 @@
+let code1
+
+code1 = 7 + 3
+//console.log(code1)
+const code2 = (100 - 20) / 2
+//console.log(code2)
+let code3 = (15%4)+36
+//console.log(code3)
+const message = "The vault has been secured. the combination is: "
+//console.log(message)
+const codeA = code1 + "-" + code2 + "-" + code3
+//console.log(codeA)
+const codeB = `${code1}-${code2}-${code3}`
+//console.log(codeB)
+console.log(message)
+console.log(codeA)
+console.log(codeB)
